2f0753576414d03a0f5c6f715fc6147c6c7750a5
hadye <hadyelzayady@yahoo.com>
2018-05-05 19:11:21 +0200

    update instruction set

--- MicroProcessor.vhd
+++ MicroProcessor.vhd
@@ -183,25 +183,44 @@ component RegisterFile is
 		);
 end component RegisterFile;
 
---instructions
-	CONSTANT  SHL  :  std_logic_vector(4 downto 0)  := "00000";
-	CONSTANT  SHR  :  std_logic_vector(4 downto 0)  := "00001";
+--------------------------------------------------------------------------------
+-- instructions opcode
+--------------------------------------------------------------------------------
+
+	CONSTANT  NOP  :  std_logic_vector(4 downto 0)  := "00000";
+	CONSTANT  MOV  :  std_logic_vector(4 downto 0)  := "00001";
 	CONSTANT  ADD  :  std_logic_vector(4 downto 0)  := "00010";--first bit must be 0 and sub is the opposite (also in inc and dec)
 	CONSTANT  SUB  :  std_logic_vector(4 downto 0)  := "00011";
-	CONSTANT  RLC  :  std_logic_vector(4 downto 0)  := "00100";
-	CONSTANT  RRC  :  std_logic_vector(4 downto 0)  := "00101";
-	CONSTANT  myAND  :  std_logic_vector(4 downto 0)  := "00110";
-	CONSTANT  myOR  :  std_logic_vector(4 downto 0)  := "00111";
-	CONSTANT  INC  :  std_logic_vector(4 downto 0)  := "01000";
-	CONSTANT  DEC  :  std_logic_vector(4 downto 0)  :=  "01001";
+	CONSTANT  myAND  :  std_logic_vector(4 downto 0)  := "00100";
+	CONSTANT  myOR  :  std_logic_vector(4 downto 0)  := "00101";
+	CONSTANT  RLC  :  std_logic_vector(4 downto 0)  := "00110";
+	CONSTANT  RRC  :  std_logic_vector(4 downto 0)  := "00111";
+	CONSTANT  SHL  :  std_logic_vector(4 downto 0)  := "01000";
+	CONSTANT  SHR  :  std_logic_vector(4 downto 0)  :=  "01001";
 	CONSTANT  SETC  :  std_logic_vector(4 downto 0)  := "01010";
 	CONSTANT  CLC  :  std_logic_vector(4 downto 0)  := "01011";
-	CONSTANT  myNOT  :  std_logic_vector(4 downto 0)  := "01100";
-	CONSTANT  NEG  :  std_logic_vector(4 downto 0)  := "01101";
-	CONSTANT  STD  :  std_logic_vector(4 downto 0)  := "01110";
-	CONSTANT  MOV  :  std_logic_vector(4 downto 0)  := "01111";
-	CONSTANT  LDM  :  std_logic_vector(4 downto 0)  := "10000";
-	CONSTANT  LDD  :  std_logic_vector(4 downto 0)  := "10001";
+	CONSTANT  PUSH  :  std_logic_vector(4 downto 0)  := "01100";
+	CONSTANT  POP  :  std_logic_vector(4 downto 0)  := "01101";
+	CONSTANT  myOUT  :  std_logic_vector(4 downto 0)  := "01110";
+	CONSTANT  myIN  :  std_logic_vector(4 downto 0)  := "01111";
+	CONSTANT  myNOT  :  std_logic_vector(4 downto 0)  := "10000";
+	CONSTANT  NEG :  std_logic_vector(4 downto 0)  := "10001";
+	CONSTANT  INC :  std_logic_vector(4 downto 0)  := "10010";
+	CONSTANT  DEC :  std_logic_vector(4 downto 0)  := "10011";
+	CONSTANT  JZ :  std_logic_vector(4 downto 0)  := "10100";
+	CONSTANT  JN :  std_logic_vector(4 downto 0)  := "10101";
+	CONSTANT  JC :  std_logic_vector(4 downto 0)  := "10110";
+	CONSTANT  JMP :  std_logic_vector(4 downto 0)  := "10111";
+	CONSTANT  CALL :  std_logic_vector(4 downto 0)  := "11000";
+	CONSTANT  RET :  std_logic_vector(4 downto 0)  := "11001";
+	CONSTANT  RTI :  std_logic_vector(4 downto 0)  := "11010";
+	CONSTANT  LDM :  std_logic_vector(4 downto 0)  := "11011";
+	CONSTANT  LDD :  std_logic_vector(4 downto 0)  := "11100";
+	CONSTANT  STD :  std_logic_vector(4 downto 0)  := "11101";
+--------------------------------------------------------------------------------
+-- end instr
+--------------------------------------------------------------------------------
+
 signal FlagRegisterWe :std_logic; -- temporary shoudl come from controller
 signal ExMemBuffWe :std_logic; -- temporary shoudl come from controller
 signal Memout :std_logic_vector(15 downto 0); -- temporary shoudl wite to MemWbBuff
@@ -415,19 +434,19 @@ begin
 					--x"0000" when others;
 	--EMPort2Buff : my_nDFF generic map (n => 16) port map(Clk,Rst,port2_data,port2_dataD);
 	Ex_MEMLabel: EXMEM_buffer port map(pcinE,spinE,aluresultinE
-,InputportinE,ImminE,EAinE,rsrcinE,rdstinE
-,opcodeinE 
-,flaginE
-,rsrcnoinE,rdstnoinE,jumpinE
-,pushpopinE,getdatafrominE,retinE
-,IDEX_rewriteE,IDEX_resetE,ClkE,wbinE,memtoreginE,memreadinE,memwriteinE,callinE,interruptinE,outportinE
-,pcoutE,spoutE,aluresultoutE
-,InputportoutE,ImmoutE,EAoutE,rsrcoutE,rdstoutE
-,opcodeoutE
-,flagoutE
-,rsrcnooutE,rdstnooutE,jumpoutE
-,pushpopoutE,getdatafromoutE,retoutE
-,wboutE,memtoregoutE,memreadoutE,memwriteoutE,calloutE,interruptoutE,outportoutE);
+		,InputportinE,ImminE,EAinE,rsrcinE,rdstinE
+		,opcodeinE 
+		,flaginE
+		,rsrcnoinE,rdstnoinE,jumpinE
+		,pushpopinE,getdatafrominE,retinE
+		,IDEX_rewriteE,IDEX_resetE,ClkE,wbinE,memtoreginE,memreadinE,memwriteinE,callinE,interruptinE,outportinE
+		,pcoutE,spoutE,aluresultoutE
+		,InputportoutE,ImmoutE,EAoutE,rsrcoutE,rdstoutE
+		,opcodeoutE
+		,flagoutE
+		,rsrcnooutE,rdstnooutE,jumpoutE
+		,pushpopoutE,getdatafromoutE,retoutE
+		,wboutE,memtoregoutE,memreadoutE,memwriteoutE,calloutE,interruptoutE,outportoutE);
 	FlagRegister : my_nDFF generic map (n => 4) port map(Clk,Rst,'1',NewFlags,FlagsOutput); 
 
 	----------------------------------------------------------------------------
